661ae018aeda5761871214ac0f87f5a40549a9e9
Mau Fournier <mau.fournier@gmail.com>
2023-10-24 20:21:11 -0600

    docs: Add corkboard example

---
+++ corkboard.example.ts
@@ -0,0 +1,10 @@
+// @ts-nocheck
+/*
+	This is your corkboard.
+
+	It's a place to put your ideas, thoughts, and notes.
+
+	I like to use it as a casual to-do list, and constantly dump new ideas into it,
+	as well as prioritizing them often.
+*/
+export {};
